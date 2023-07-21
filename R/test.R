d8fa821422952aad431c09ef76aa78d610d27ca5
Shinichi Nakagawa <s.nakagawa@unsw.edu.au>
2023-07-21 16:43:27 +0900

    Update test.R

--- R/test.R
+++ R/test.R
@@ -6,14 +6,14 @@ library(orchaRd)
 library(metafor)
 
 # get data
-
-# prey
 dat_prey <- read_csv(here("data/prey_19072023.csv"))
 dat_pred <- read_csv(here("data/predator_19072023.csv"))
 dim(dat_prey)
 dim(dat_pred)
 
-names(dat_prey)
+##########
+# prey
+##########
 
 # check whether "T_proportion" and "C_proportion" have 0 or not
 dat_prey %>%
@@ -53,3 +53,129 @@ dat_prey %>%
 dat_prey %>%
   filter(T_sd == 0 | C_sd == 0) %>%
   select(T_sd, C_sd)
+
+
+dat1 <- effect_lnRR_prey(dat_prey)
+
+hist(dat1$lnRR) 
+
+# TODO something
+hist(dat1$lnRR_var)
+
+# this looks strange
+# TODO check 83rd row
+which(dat1$lnRR == min(dat$lnRR))
+
+hist(dat1$lnRR[-83])
+hist(log(dat1$lnRR_var[-83]))
+
+# meta-analysis
+# TODO - get phylo, VCV and other things
+dat1$Obs_ID <- 1:nrow(dat1)
+dat1 <- dat1[-83, ]
+
+ma_prey <- rma.mv(yi = lnRR,
+       V = lnRR_var, 
+       random = list(~1 | Study_ID,
+                     #~1 | Cohort_ID, #TODO  some missing values
+                     ~1 | Obs_ID),
+       #R = list(Phylo = cor_tree),
+       test = "t",
+       method = "REML", 
+       sparse = TRUE,
+       data = dat1)
+
+summary(ma_prey)
+i2_ml(ma_prey)
+
+
+orchard_plot(ma_prey,
+             group = "Study_ID",
+             xlab = "log response ratio (lnRR)")
+
+# meta-regression
+mr_prey <- rma.mv(yi = lnRR,
+       V = lnRR_var, 
+       mods = ~ Treatment_stimulus - 1,
+       random = list(~1 | Study_ID,
+                     #~1 | Cohort_ID, #TODO  some missing values
+                     ~1 | Obs_ID),
+       #R = list(Phylo = cor_tree),
+       test = "t",
+       method = "REML", 
+       sparse = TRUE,
+       data = dat1)
+
+summary(mr_prey)
+
+
+orchard_plot(mr_prey,
+             mod = "Treatment_stimulus",
+             group = "Study_ID",
+             xlab = "log response ratio (lnRR)")
+
+##########
+# predator
+##########
+# check whether "T_mean" and "C_mean" have 0 or not
+dat_pred %>%
+  filter(T_mean == 0 | C_mean == 0) %>%
+  select(T_mean, C_mean)
+
+# repliace 0 in "C_mean" with 0.01
+# TODO - check whether this is correct
+dat_pred <- dat_pred %>%
+  mutate(C_mean = ifelse(C_mean == 0, 0.01, C_mean))
+
+dat2 <- effect_lnRR_predator(dat_pred)
+
+hist(dat2$lnRR) 
+hist(dat2$lnRR_var)
+
+# meta-analysis
+dat2$Obs_ID <- 1:nrow(dat2)
+
+ma_pred <- rma.mv(yi = lnRR,
+       V = lnRR_var, 
+       random = list(~1 | Study_ID,
+                     #~1 | Cohort_ID, #TODO  some missing values
+                     ~1 | Obs_ID),
+       #R = list(Phylo = cor_tree),
+       test = "t",
+       method = "REML", 
+       sparse = TRUE,
+       data = dat2)
+
+summary(ma_pred)
+i2_ml(ma_pred)
+
+
+orchard_plot(ma_pred,
+             group = "Study_ID",
+             xlab = "log response ratio (lnRR)")
+
+# meta-regression
+# TODO Treatment_stimulus - mistake in there
+# fixing Treatment_stimulus - merge "eye spot" and "eyespot"
+dat2$Treatment_stimulus <- 
+  ifelse(dat2$Treatment_stimulus == "eye spots", "eyespots", dat2$Treatment_stimulus)
+
+mr_pred <- rma.mv(yi = lnRR,
+       V = lnRR_var, 
+       mods = ~ Treatment_stimulus,
+       random = list(~1 | Study_ID,
+                     #~1 | Cohort_ID, #TODO  some missing values
+                     ~1 | Obs_ID),
+       #R = list(Phylo = cor_tree),
+       test = "t",
+       method = "REML", 
+       sparse = TRUE,
+       data = dat2)
+
+summary(mr_pred)
+
+
+orchard_plot(mr_pred,
+             mod = "Treatment_stimulus",
+             group = "Study_ID",
+             xlab = "log response ratio (lnRR)")
