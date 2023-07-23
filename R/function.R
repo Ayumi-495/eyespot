f3951b5a6e50317f1f323bb4729e507276041200
Ayumi-495 <cawcaw1701@gmail.com>
2023-07-23 23:48:51 +1000

    update
    continue working

--- R/function.R
+++ R/function.R
@@ -1,15 +1,16 @@
 
-library(easypackages)
-libraries(c("metafor", "tidyverse", "gt"))
+library(metafor)
+library(tidyverse)
+
+# TODO insert - when "C_mean = 0, C_proportion = 0, C_sd = 0" replace 0.01 or minimum values using ifelse()
 
 
-##################### create original function for predator dataset #####################
-## continuous data, proportion1 (proportion_all), proportion2 (proportion_bird))
-
-effect_lnRR_predator <- function(dt) 
-{
-
-  ## copy dataset for adding effect size and its variation (lnRR /lnRR_var) column
+## function for predator dataset ##
+effect_lnRR_pred <- function(dt) 
+  {
+  
+# copy dataset for adding effect size and 
+# its variation (lnRR /lnRR_var) column
   dt1 <- dt %>% 
     mutate(lnRR     = NA,
            lnRR_var = NA)
@@ -18,17 +19,18 @@ effect_lnRR_predator <- function(dt)
     
     Tn <- dt1$Tn[i]
     Cn <- dt1$Cn[i]
+    T_proportion <- dt1$T_proportion[i]
+    C_proportion <- dt1$C_proportion[i]
     T_mean <- dt1$T_mean[i]
     C_mean <- dt1$C_mean[i]
     T_sd <- dt1$T_sd[i]
     C_sd <- dt1$C_sd[i]
-    Measure <- dt1$Measure[i]
     Response <- dt1$Response[i]
     
-    ## continuous data - using escalc() (metafor package)
+    # continuous data - using escalc() (metafor package)
     if (Response == "continuous") {
       
-      ### reverse means when latency was measured
+      # reverse means when latency was measured
       T_mean <- replace(T_mean, Measure == "latency",
                         T_mean[Measure == "latency"] * (-1))
       C_mean <- replace(C_mean, Measure == "latency",
@@ -46,53 +48,47 @@ effect_lnRR_predator <- function(dt)
       
     }
     
-    ## proportion data (bird treatment group vs. control group)
+    # proportion data (no sd values)
     else if (Response == "proportion1") {
       
-      ### transform proportion value
-      asin_trans <- function(proportion) {asin(sqrt(proportion))}
+      # transform proportion value
+      asin_trans <- function(proportion) {
+        trans <- asin(sqrt(proportion)) 
+        trans
+      }
       
-      T_mean <- asin_trans(T_mean)
-      C_mean <- asin_trans(C_mean)
+      T_proportion <- asin_trans(T_proportion)
+      C_proportion <- asin_trans(C_proportion)
       
-      ### calculate lnRR and lnRR variance   
-      # Fixed - () wrong
-      lnRR_pro1 <- log(T_mean / C_mean)
-      lnRR_var_pro1 <- (1 / sqrt(8))^2 * (1 / (T_mean^2 * Tn) +
-                                            1 / (C_mean^2 * Cn))
+      # calculate lnRR and lnRR variance   
+      lnRR_pro1 <- log(T_proportion / C_proportion)
+      lnRR_var_pro1 <- (1 / sqrt(8))^2 * (1 / (T_proportion^2 * Tn) +
+                                            1 / (C_proportion^2 * Cn))
       
       dt1$lnRR[i] <- lnRR_pro1
       dt1$lnRR_var[i] <- lnRR_var_pro1
       
     }
     
-    ## proportion data (within single group) 
-    ## For example… 
-    ## if the sample size is 30 (n = 40), 10 individuals attacked when they saw the treatment (0.25 (25%)), 
-    ## but 30 individuals attacked when they saw the control (0.75 (75%)).
-    
+   # proportion data (exist sd values) 
     else if (Response == "proportion2") {
       
-      ### transform proportion mean value
+      # transform proportion mean value
       asin_trans <- function(proportion) {
          trans <- asin(sqrt(proportion)) 
          trans
          }
       
-      # TODO this was wrong
-      #T_SD <- sqrt((1/sqrt(8))^2/(4*(T_mean)*(1-(T_mean))))
-      #C_SD <- sqrt((1/sqrt(8))^2/(4*(C_mean)*(1-(C_mean))))
-      T_SD <- T_sd^2/(4*(T_mean)*(1-(T_mean)))
-      C_SD <- C_sd^2/(4*(C_mean)*(1-(C_mean)))
+      T_SD <- T_sd^2/(4*(T_proportion)*(1-(T_proportion)))
+      C_SD <- C_sd^2/(4*(C_proportion)*(1-(C_proportion)))
 
-      T_mean <- asin_trans(T_mean)
-      C_mean <- asin_trans(C_mean)
+      T_proportion <- asin_trans(T_proportion)
+      C_proportion <- asin_trans(C_proportion)
       
-      ### calculate lnRR and lnRR variance 
-      # () was wrong
-      lnRR_pro2 <- log(T_mean / C_mean)
-      lnRR_var_pro2 <- (T_SD)^2 * (1 / (T_mean^2 * Tn)) +
-        (C_SD)^2 * (1 / (C_mean^2 * Cn))
+      # calculate lnRR and lnRR variance 
+      lnRR_pro2 <- log(T_proportion / C_proportion)
+      lnRR_var_pro2 <- (T_SD)^2 * (1 / (T_proportion^2 * Tn)) +
+                       (C_SD)^2 * (1 / (C_proportion^2 * Cn))
       
       dt1$lnRR[i] <- lnRR_pro2
       dt1$lnRR_var[i] <- lnRR_var_pro2
@@ -104,13 +100,11 @@ effect_lnRR_predator <- function(dt)
   
 }
 
-#####################  create original function for prey dataset #####################
-
+## function for prey dataset ##
 effect_lnRR_prey <- function(dt) 
-{
+  {
   
-  ## copy dataset for adding effect size (lnRR /lnRR_var) column
-  dt1 <- dt %>% 
+    dt1 <- dt %>% 
     mutate(lnRR     = NA,
            lnRR_var = NA)
   
@@ -126,7 +120,7 @@ effect_lnRR_prey <- function(dt)
     C_sd <- dt1$C_sd[i]
     Response <- dt1$Response[i]
     
-    ## continuous data - using escalc() (metafor package)
+   # continuous data - using escalc() (metafor package)
     if (Response == "continuous") {
       
       effect <- escalc(measure = "ROM", 
@@ -141,24 +135,49 @@ effect_lnRR_prey <- function(dt)
       
     }
     
-    ## proportion data (treatment group vs. control group)
-    else if (Response == "proportion") {
+    # proportion data (no sd values)
+    else if (Response == "proportion1") {
       
-      ### transform proportion value
-      asin_trans <- function(proportion) {asin(sqrt(proportion))}
+      # transform proportion mean value
+      asin_trans <- function(proportion) {
+        trans <- asin(sqrt(proportion)) 
+        trans
+      }
       
       T_proportion <- asin_trans(T_proportion)
       C_proportion <- asin_trans(C_proportion)
       
-      ### calculate lnRR and lnRR variance   
+      # calculate lnRR and lnRR variance   
       lnRR_pro1 <- log(T_proportion / C_proportion)
-      #lnRR_var_pro1 <- (1 / sqrt(8))^2 * (1 / (T_proportion^2) * Tn + 
-                                            #1 / (C_proportion^2) * Cn)
       lnRR_var_pro1 <- (1 / sqrt(8))^2 * (1 / (T_proportion^2 * Tn) + 
                                             1 / (C_proportion^2 * Cn))
       
       dt1$lnRR[i] <- lnRR_pro1
       dt1$lnRR_var[i] <- lnRR_var_pro1
+      
+    }
+    
+    # proportion data (exist sd values) 
+    else if (Response == "proportion2") {
+      
+      asin_trans <- function(proportion) {
+        trans <- asin(sqrt(proportion)) 
+        trans
+      }
+      
+      T_SD <- T_sd^2/(4*(T_proportion)*(1-(T_proportion)))
+      C_SD <- C_sd^2/(4*(C_proportion)*(1-(C_proportion)))
+      
+      T_proportion <- asin_trans(T_proportion)
+      C_proportion <- asin_trans(C_proportion)
+      
+      # calculate lnRR and lnRR variance 
+      lnRR_pro2 <- log(T_proportion / C_proportion)
+      lnRR_var_pro2 <- (T_SD)^2 * (1 / (T_proportion^2 * Tn)) +
+                       (C_SD)^2 * (1 / (C_proportion^2 * Cn))
+      
+      dt1$lnRR[i] <- lnRR_pro2
+      dt1$lnRR_var[i] <- lnRR_var_pro2
       
     }
   }
@@ -167,14 +186,3 @@ effect_lnRR_prey <- function(dt)
   
 }
 
-##################### check original functions #####################
-
-## predator dataset - import predator_19072023.csv from data folder
-test_lnRR_predator  <- effect_lnRR_predator(predator)
-
-## prey dataset - import prey_19072023.csv from data folder
-test_lnRR_prey <- effect_lnRR_prey(prey)
-
-## see tables
-test_lnRR_predator %>% gt()
-test_lnRR_prey     %>% gt()
